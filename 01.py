8dd74c5ecd30aa18f40bc6a3ddc91174fe4cc3bf
김여준 <kt65346337@gmail.com>
2024-01-24 22:50:34 +0900

    객체지향 보완

---
+++ 01.py
@@ -0,0 +1,16 @@
+V = int(input())
+string = input()
+
+# countA = lst.count('A')
+# countB = lst.count('B')
+
+# if countA > countB :
+#     print('A')
+
+# elif countA < countB :
+#     print('B')
+
+# else :
+#     print('Tie')
+# print(countB)
+print(string)
